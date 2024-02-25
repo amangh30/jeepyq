71e1e5ef32ea35dea097a6a1c336406e84509abc
Aman Sinha <amansinha561@gmail.com>
2024-02-25 16:18:53 +0530

    added admin functionality

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -5,20 +5,44 @@ import "react-toastify/dist/ReactToastify.css";
 import { useState } from 'react';
 import "../style/admin.css"
 import axios from "axios";
+import Input from '@mui/material/Input';
 
-
-const Input = styled(TextField)`
+const In = styled(TextField)`
         margin-top:30px;
         font-size:20px
     `
-    const Btn = styled(Button)`
+const I = styled(Input)`
+    width:220px;
+    border:2px solid grey;
+    color:black;
+    background-color:white;
+`
+
+const Btn = styled(Button)`
         margin-top:30px;
         border-radius:22px;
     ` 
 
 const Admin = ()=>{
+    const [file, setFile] = useState(null);
+    const [year, setYear] = useState('');
+    const [date, setDate] = useState('');
+    const [shift, setShift] = useState('');
+
     const [isNotVerified,setisNotVerified] = useState(true);
     const [password,setPassword] = useState('')
+    const handleYearChange = (e) => {
+        setYear(e.target.value);
+    };
+    const handleDateChange = (e) => {
+        setDate(e.target.value);
+    };
+    const handleShiftChange = (e) => {
+        setShift(e.target.value);
+    };
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
     const handleChange=(e)=>{
         setPassword(e.target.value)
     }
@@ -42,6 +66,29 @@ const Admin = ()=>{
             console.log(e)
         }
     }
+    const handleSubmit = async () => {
+        const formData = new FormData();
+        formData.append('year', year);
+        formData.append('date', date);
+        formData.append('shift', shift);
+        formData.append('file', file);
+
+        try {
+            await axios.post('http://localhost:8000/add', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            setYear('')
+            setDate('')
+            setShift('')
+            setFile(null)
+            alert('Collection uploaded successfully');
+        } catch (error) {
+            console.error('Error uploading collection:', error);
+            alert('Error uploading collection');
+        }
+    };
 
     return(
         <>
@@ -55,7 +102,7 @@ const Admin = ()=>{
             </div>
             <div className='text'>
             <p>Login To Continue</p>
-            <Input value={password} onChange={handleChange} id="standard-basic" label="Enter The Password" type='password' variant="standard" />
+            <In value={password} onChange={handleChange} id="standard-basic" label="Enter The Password" type='password' variant="standard" />
             </div>
             <div className='btn'>
             <Btn onClick={handleClick} variant="contained">Submit</Btn>
@@ -63,7 +110,20 @@ const Admin = ()=>{
         </div>
         <ToastContainer />
         </div>
-         ):(<p>Welcome To Admin Page</p>)}
+         ):(
+            <div className='adm'>
+            <div className='welcome'>Welcome To Admin Panel</div>
+            <div className='fl'>
+                <I type="text" value={year} placeholder='Enter Year'  onChange={handleYearChange} required />
+                <I type="date" value={date} placeholder='Enter Date' onChange={handleDateChange} required />
+                <I type="text" value={shift} placeholder='Enter Shift' onChange={handleShiftChange} required/>
+                <I type='file' required onChange={handleFileChange}/>
+            </div>
+            <div className='btn'>
+            <Btn onClick={handleSubmit} variant="contained">Submit</Btn>
+            </div>            
+            </div>
+         )}
         </>
     )
 }
